e10fe6d157019953b3a59fd0fc38d1836095f0e0
Vansh13199 <vansh25bcs80031@gmail.com>
2026-04-30 14:51:48 +0530

    endsem

---
+++ Leetcode-Set1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+      map<int, int> mpp;
+      mpp[0]=1;
+      int prefix=0, cnt=0;
+      for(int i=0;i<nums.size();i++){
+        prefix+=nums[i];
+        int remove=prefix-k;
+        cnt+=mpp[remove];
+        mpp[prefix]+=1;
+      }
+     return cnt;   
+    }
+};
